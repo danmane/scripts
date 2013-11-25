83554362b372d1f57b6ba335ba5fa27bffd9f6d9
Daniel Mane <danmane@gmail.com>
2013-11-24 23:23:45 -0600

    Seems to be working, as yet untested :)

--- easybackport.py
+++ easybackport.py
@@ -1,24 +1,25 @@
 import subprocess, re
 
 BACKPORT_BRANCH_NAME = "4.1.1-proposed"
+PUSH_LOCATION = "HEAD:refs/for/" + BACKPORT_BRANCH_NAME
 
 def main():
     commit = subprocess.check_output(["git", "log", "-n", "1",])
     chash = subprocess.check_output(["git", "log", "-n", "1", "--format=format:%H"])
     cmessage = subprocess.check_output(["git", "log", "-n", "1", "--format=format:%B"])
-    print "Backport " + chash + " to " + BACKPORT_BRANCH_NAME + "?"
+    print "Backport this commit to " + BACKPORT_BRANCH_NAME + "?"
     print "======================================"
     print commit
     print "======================================"
     backport = get_yes_no()
     if backport:
-        new_name = chash + "-on-" + BACKPORT_BRANCH_NAME
+        new_name = "bp-" + chash[0:5] + "-to-" + BACKPORT_BRANCH_NAME
         upstream = "origin/" + BACKPORT_BRANCH_NAME
         safeCall(["git", "checkout", "-b", new_name, upstream])
         safeCall(["git", "cherry-pick", chash])
-        new_message = get_new_message(cmessage)
-        safeCall(["git", "commit", "--amend", "-m", newMessage])
-
+        new_message = gen_backport_message(cmessage)
+        safeCall(["git", "commit", "--amend", "-m", new_message])
+        safeCall(["git", "push", "origin", PUSH_LOCATION])
 
 def gen_backport_message(cmessage):
     """Given the commit message, construct a new message, replacing the review string
@@ -29,12 +30,13 @@ def gen_backport_message(cmessage):
         NOCR: <reason>
     succeed iff there is exactly 1 possible review string
     """
-    p1 = "AUTOREVIEW"
-    p2 = "CR: ?(\w+,?)+"
-    p3 = "CRR: ?(\w+,?)+"
-    p4 = "NOCR: ?\S*"
+    p1 = "AUTOREVIEW ?\n"
+    p2 = "CR: ?(\w+,? ?)+\n"
+    p3 = "CRR: ?(\w+,? ?)+\n"
+    p4 = "NOCR: ?\S* ?\n"
     patterns = [p1, p2, p3, p4]
     search = "|".join(patterns)
+    # print search
     num_results = len(re.findall(search, cmessage))
     if num_results == 0:
         print "Error: no valid review string found. Amend the commit message yourself, then push"
@@ -42,9 +44,10 @@ def gen_backport_message(cmessage):
     elif num_results > 1:
         print "Error: multiple possible review strings found. Amend the commit message yourself, then push"
         raise ValueError
-    return re.sub(search, "NOCR: backport", cmessage)
+    return re.sub(search, "NOCR: backport\n", cmessage)
 
 def safeCall(args):
+    # return
     exitVal = subprocess.call(args)
     if exitVal != 0:
         print "There were problems with this command:"
@@ -55,9 +58,11 @@ def get_yes_no():
     print "y/n:",
     response = raw_input()
     response = response.strip().lower()
-    if response == "y" || response == "yes":
-        return true
-    elif response == "n" || response == "no":
-        return false
-    else
+    if response == "y" or response == "yes":
+        return True
+    elif response == "n" or response == "no":
+        return False
+    else:
         return get_yes_no()
+
+main()
