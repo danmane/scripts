1ab17928907e58d303cee9e3abcc26a266ab5da3
Daniel Mane <danmane@gmail.com>
2014-04-01 19:33:44 -0700

    gitstats now gets file-by-file data, and is much cleaner

--- parse-gitstats.py
+++ parse-gitstats.py
@@ -1,71 +1,47 @@
 #!/usr/bin/env python
 import subprocess, re, sys, os, json
 
-# if (len(sys.argv) > 1):
-#   fileName = sys.argv[1]
-#   with open(fileName, "r") as f:
-#     contents = f.read()
-# else:
-
 def main():
   contents = subprocess.check_output(\
-      ["git", "log", "--shortstat", "--no-merges", '--format="%ae %ad"', '--reverse'])
+      ["git", "log", "--numstat", "--no-merges", '--format="%ae %ad"', '--reverse'])
 
-  contents = contents.split("\n")
-  contents = filter(lambda x: x != "", contents)
-  commits = get_commits(contents)
+  headstr = r"""(\w+)@\w+\.\w+ ([^"\n]+)"\n\n"""
+  linestr = r"\d+\t\d+\t[^\n]+\n"
+  fullstr = headstr + r"((?:" + linestr + r")*)"
 
-  print(json.dumps([safe_parse_commit(c) for c in commits], indent=2))
+  commits = [parse_commit(cGroup) for cGroup in re.finditer(fullstr, contents)]
+  
+  print(json.dumps(commits, indent=2))
 
-def get_commits(lines):
-  current_commit = []
-  commits = []
-  for l in lines:
-    if re.match("\"\w+@\w+\.\w+", l):
-      if len(current_commit) == 2:
-        commits.append(current_commit)
-      current_commit = [l]
+def parse_commit(cGroup):
+  g = cGroup.groups()
+  line_grouper = r"(\d+)\t(\d+)\t([^\n]+)\n"
+  files_changed = [parse_line(lGroup) for lGroup in re.finditer(line_grouper, g[2])]
+  return {"name": g[0], "date": g[1], "files": files_changed}
+
+def parse_line(lGroup):
+  g = lGroup.groups()
+  return {"additions": g[0],
+          "deletions": g[1],
+          "fileName" : g[2],
+          "directory": getDirectory(g[2]),
+          "extension": getExtension(g[2])}
+
+def getDirectory(fName):
+  m = re.match("(\w+)/", fName)
+  if m:
+    return m.groups()[0]
+  else:
+    return ""
+
+def getExtension(fName):
+  if fName[-5:] == ".d.ts":
+    return "d.ts"
+  else:
+    (a,b,c) = fName.rpartition(".")
+    if a == "":
+      return ""
     else:
-      current_commit.append(l)
-  if len(current_commit) == 2:
-    commits.append(current_commit)
-
-  return commits
-
-
-def safe_parse_commit(c):
-  try:
-    return parse_commit(c)
-  except Exception as e:
-    print(e)
-    print(c)
-    print("\n=========\n")
-    return {}
-
-def parse_commit(c):
-  out = {}
-  topline = c[0]
-  assert(topline[0] == '"')
-  topline = topline[1:-1]
-  botline = c[1]
-  email, _, date = topline.partition(" ")
-  # print("email", email)
-  out["email"] = email
-  out["name"] = email.partition('@')[0]
-  # print("date", date)
-  out["date"] = date
-  botline = botline.split(",")
-  changed = botline[0].strip().partition(" ")[0]
-  out["changed"] = int(changed)
-  # print("changed", changed)
-  insertions = botline[1].strip().partition(" ")[0]
-  out["insertions"] = int(insertions)
-  # print("insertions", insertions)
-  try:
-    deletions = botline[2].strip().partition(" ")[0]
-    out["deletions"] = int(deletions)
-  except:
-    out["deletions"] = 0
-  return out
+      return c
 
 main()
