2708820b9430203ea856dd07a0025be271d57982
Daniel Mane <danmane@gmail.com>
2014-04-01 20:44:55 -0700

    It now aggregates by directory, extension, etc

--- parse-gitstats.py
+++ parse-gitstats.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-import subprocess, re, sys, os, json
+import subprocess, re, json, itertools, pdb
 
 def main():
   contents = subprocess.check_output(\
@@ -10,19 +10,59 @@ def main():
   fullstr = headstr + r"((?:" + linestr + r")*)"
 
   commits = [parse_commit(cGroup) for cGroup in re.finditer(fullstr, contents)]
-  
-  print(json.dumps(commits, indent=2))
+  changes = [c for commit in commits for c in commit["changes"]]
+  changesByFile = groupAndMergeChangesByAggregator(changes, "fileName", ["fileName", "directory", "extension"])
+  changesByDirectory = groupAndMergeChangesByAggregator(changes, "directory")
+  changesByExtension = groupAndMergeChangesByAggregator(changes, "extension")
+  # changesByName = groupAndMergeChangesByAggregator(changes, "name")
+  out = {"commits": commits, "files": changesByFile, "directory": changesByDirectory, "extension": changesByExtension}
+
+  print(json.dumps(out, indent=2))
+
+def groupBy(ls, keyFn):
+  m = {}
+  for l in ls:
+    k = keyFn(l)
+    if k not in m:
+      m[k] = []
+    m[k].append(l)
+  return m.values()
+
+def keyfn(k): return lambda d: d[k]
+
+def groupAndMergeChangesByAggregator(changes, aggregator, invariants=None):
+  if invariants == None:
+    invariants = [aggregator]
+  groupedChanges = groupBy(changes, keyfn(aggregator))
+  return [mergeChanges(g, invariants) for g in groupedChanges]
+
+def mergeChanges(cs, invariants):
+  # Takes a list of changes
+  additions = sum(map(keyfn("additions"), cs))
+  deletions = sum(map(keyfn("deletions"), cs))
+  lines = additions - deletions
+  out = {"additions": additions, "deletions": deletions, "lines": lines}
+  for iv in invariants:
+    ivs = map(keyfn(iv), cs)
+    assert(allEqual(ivs))
+    out[iv] = ivs[0]
+  return out
+
+def allEqual(l):
+  return all(map(lambda x: x==l[0], l))
 
 def parse_commit(cGroup):
   g = cGroup.groups()
   line_grouper = r"(\d+)\t(\d+)\t([^\n]+)\n"
-  files_changed = [parse_line(lGroup) for lGroup in re.finditer(line_grouper, g[2])]
-  return {"name": g[0], "date": g[1], "files": files_changed}
+  changes = [parse_line(lGroup) for lGroup in re.finditer(line_grouper, g[2])]
+  byDirectory = groupAndMergeChangesByAggregator(changes, "directory")
+  byExtension = groupAndMergeChangesByAggregator(changes, "extension")
+  return {"name": g[0], "date": g[1], "changes": changes, "byDirectory": byDirectory, "byExtension": byExtension}
 
 def parse_line(lGroup):
   g = lGroup.groups()
-  return {"additions": g[0],
-          "deletions": g[1],
+  return {"additions": int(g[0]),
+          "deletions": int(g[1]),
           "fileName" : g[2],
           "directory": getDirectory(g[2]),
           "extension": getExtension(g[2])}
